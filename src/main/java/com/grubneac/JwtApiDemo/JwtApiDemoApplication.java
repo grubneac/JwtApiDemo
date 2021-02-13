9a10f9b1f3aa11b7f2180d83fc95ad75da30d7a7
Grubneac <grubneac@yandex.ru>
2021-02-13 10:23:43 +0200

    Initial commit

---
+++ src/main/java/com/grubneac/JwtApiDemo/JwtApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.grubneac.JwtApiDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grubneac/JwtApiDemo/JwtApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grubneac.JwtApiDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
